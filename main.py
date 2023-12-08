39922637cebbf2754b280132bb88b0c0d1ca58c2
gcmgabrielmartinez <gabrielcalvo2812@gmail.com>
2023-12-08 14:31:20 +0000

    Initial commit

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ rect.set_width(3)
 print(rect.get_perimeter())
 print(rect)
 
+print(rect.get_picture())
+
 sq = shape_calculator.Square(9)
 print(sq.get_area())
 sq.set_side(4)
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,58 @@
 class Rectangle:
+  def __init__(self, width:int, height:int):
+    self.height = height
+    self.width = width
+
+  def get_area(self):
+    return self.height * self.width
+
+  def get_perimeter(self):
+    return 2*self.height + 2*self.width
+
+  def get_diagonal(self):
+    return (self.height**2 + self.width**2)**0.5
+  
+  #@width.setter
+  def set_width(self, width):
+    self.width = width
+  
+  #@height.setter
+  def set_height(self, height):
+    self.height = height
+
+  def __str__(self) -> str:
+    return f"Rectangle(width={self.width}, height={self.height})"
+
+  def get_picture(self):
+    if self.height > 50 or self.width > 50:
+      return "Too big for picture."
+    else:
+      picture = ""
+      for i in range(self.height):
+        picture += "*"*self.width + "\n"
+      return picture
+
+  def get_amount_inside(self, shape):
+    return int(self.height/shape.height) * int(self.width/shape.width)
 
 
+class Square(Rectangle):
+  def __init__(self, side:int):
+    super().__init__(side, side)
 
+  def set_side(self, side):
+    self.width = side
+    self.height = side
+ 
+  #@width.setter
+  def set_width(self, width):
+    self.width = width
+    self.height = width
 
-class Square:
+#@height.setter
+  def set_height(self, height):
+    self.width = height
+    self.height = height
+
+  def __str__(self) -> str:
+    return f"Square(side={self.height})"
